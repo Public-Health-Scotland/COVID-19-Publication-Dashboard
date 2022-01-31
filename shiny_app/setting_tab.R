403f6a0c980a7fdd6d28c990424b4026021f8740
RosalynLP <rosalyn.pearson@phs.scot>
2022-01-31 11:54:10 +0000

    Capitalise week ending

--- shiny_app/setting_tab.R
+++ shiny_app/setting_tab.R
@@ -16,19 +16,19 @@ observeEvent(input$btn_dataset_modal3,
 output$Setting_explorer <- renderUI({
 
    tagList(
-          actionButton("btn_dataset_modal3", paste0("Data source: Data source: Case Management System"), icon = icon('question-circle'))) 
-          
+          actionButton("btn_dataset_modal3", paste0("Data source: Data source: Case Management System"), icon = icon('question-circle')))
+
   # Charts and rest of UI
-  if (input$Setting_select == "All setting types") { #  
+  if (input$Setting_select == "All setting types") { #
     tagList(h3("Settings"),
             actionButton("btn_dataset_modal3", paste0("Data source: Case Management System"), icon = icon('question-circle')),
       plot_box("Cases by setting type", plot_output = "SettingType"),
      h4("Data Table"),
      dataTableOutput("setting_table")
     )
-    
 
-  }  else if (input$Setting_select != "All setting types" ) { # drill down to setting locations 
+
+  }  else if (input$Setting_select != "All setting types" ) { # drill down to setting locations
     tagList(h3("Settings"),
             actionButton("btn_dataset_modal3", paste0("Data source: Case Management System"), icon = icon('question-circle')),
     p("Single click on an item in the legend to remove it from the plot."),
@@ -37,18 +37,20 @@ output$Setting_explorer <- renderUI({
     h4("Data Table"),
     dataTableOutput("setting_table")
     )
-  }  
-}) 
-  
+  }
+})
+
 output$SettingType <- renderPlotly({plot_settings_chart(Settings, data_name = "Settings", settingdata = "Type")})
 output$SettingLocation <- renderPlotly({plot_settings_chart(Settings, data_name = "Settings", settingdata = "Location")})
 
 ## Data view -----
 
 output$setting_table <- DT::renderDataTable({
-  
+
+  Settings %<>% dplyr::rename("Week Ending" = "week_ending")
+
   datatab_table(Settings, add_separator_cols = 4)
-  
+
 })
 
 
