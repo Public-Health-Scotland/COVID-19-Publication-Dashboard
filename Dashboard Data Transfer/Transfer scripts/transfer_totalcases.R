e4098468b8e41debcd80b60ec25a9fbf7dfb4986
RosalynLP <rosalyn.pearson@phs.scot>
2022-02-09 11:06:42 +0000

    load total cases from daily data

---
+++ Dashboard Data Transfer/Transfer scripts/transfer_totalcases.R
@@ -0,0 +1,21 @@
+# Dashboard data transfer for Total Daily Cases
+# Sourced from ../dashboard_data_transfer.R
+
+##### 20. Total daily cases
+
+i_dailydata <- read_all_sheets("Input data/DailyDataFigs.xlsx")
+
+### a) Total cases
+
+pop_grandtotal <- i_population$pop_number[i_population$age_group == "All" & i_population$sex == "Total"]
+
+g_cases <- i_dailydata$Cases %>%
+  dplyr::rename(NumberCasesperDay = `Cases per day`,
+                Average7 = `7 Day Average`) %>%
+  mutate(CumulativeRatePer10000 = 10000 * Cumulative / pop_grandtotal) %>%
+  select(Date, NumberCasesperDay, Cumulative, Average7, CumulativeRatePer10000)
+
+
+write.csv(g_cases, glue("Test output/TotalCases.csv"), row.names = FALSE)
+
+rm(g_cases, pop_grandtotal)
