394ee849bb4e55ee1bfad2f34e20d6c730909768
RosalynLP <rosalyn.pearson@phs.scot>
2022-02-22 13:20:34 +0000

    new ethnicity categories

--- Dashboard Data Transfer/Transfer scripts/transfer_ethnicity.R
+++ Dashboard Data Transfer/Transfer scripts/transfer_ethnicity.R
@@ -24,24 +24,10 @@ write.csv(g_ethnicity, glue("Test output/Ethnicity.csv"), row.names = FALSE)
 ### b) Ethnicity Chart
 
 g_ethnicitychart <- g_ethnicity %>%
+  dplyr::rename("Ethnicity" = "Ethnic_group") %>% 
   pivot_wider(names_from = Ethnicity, values_from = c(Admissions, Percentage)) %>%
-  dplyr::rename(
-                White_c = `Admissions_White`,
-                White_p = `Percentage_White`,
-                `Black/Caribbean/African_c` = `Admissions_Black/Caribbean/African`,
-                `Black/Caribbean/African_p` = `Percentage_Black/Caribbean/African`,
-                `South Asian_c` = `Admissions_South Asian`,
-                `South Asian_p` = `Percentage_South Asian`,
-                Chinese_c = `Admissions_Chinese`,
-                Chinese_p = `Percentage_Chinese`,
-                Other_c = `Admissions_Other`,
-                Other_p = `Percentage_Other`,
-                `Not Available_c` = `Admissions_Not Available`,
-                `Not Available_p` = `Percentage_Not Available`
-                ) %>%
-  select(names(o_ethnicitychart))
+  clean_names()
 
-g_ethnicitychart$`Month beginning` <- as.Date(format(as.Date(g_ethnicitychart$`Month beginning`, format="%d/%m/%Y"), format="%Y-%m-%d"))
 
 write.csv(g_ethnicitychart, glue("Test output/Ethnicity_Chart.csv"), row.names = FALSE)
 
