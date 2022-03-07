b518164c7835f343c9c215dd7190d032310a9744
RosalynLP <rosalyn.pearson@phs.scot>
2022-03-07 13:09:21 +0000

    other typos

--- shiny_app/ui.R
+++ shiny_app/ui.R
@@ -220,7 +220,7 @@ tagList(  #needed for shinyjs
                  p(strong(style="color:black", "From 01 March 2022, PHS now include episodes of reinfection within COVID-19 reporting.
                           Prior to this date COVID-19 cases were based on an individual’s first positive test result only.
                           The new daily calculation includes both new infections and possible reinfections.
-                          Possible reinfections are defined as individuals who tests positive, by PCR (polymerase chain reaction) or LFD (lateral flow device), 90 days or more after their last positive test.
+                          Possible reinfections are defined as individuals who test positive, by PCR (polymerase chain reaction) or LFD (lateral flow device), 90 days or more after their last positive test.
                           Currently hospital admissions do not include reinfections, although this will be updated in coming weeks.",
                           "More information available on the Public Health Scotland website",
                           tags$a(href="https://publichealthscotland.scot/news/2022/february/covid-19-reporting-to-include-further-data-on-reinfections/",
@@ -273,7 +273,7 @@ tagList(  #needed for shinyjs
       p(strong(style="color:black", "From 01 March 2022, PHS now include episodes of reinfection within COVID-19 reporting.
                           Prior to this date COVID-19 cases were based on an individual’s first positive test result only.
                           The new daily calculation includes both new infections and possible reinfections.
-                          Possible reinfections are defined as individuals who tests positive, by PCR (polymerase chain reaction) or LFD (lateral flow device), 90 days or more after their last positive test.
+                          Possible reinfections are defined as individuals who test positive, by PCR (polymerase chain reaction) or LFD (lateral flow device), 90 days or more after their last positive test.
                           Currently hospital admissions do not include reinfections, although this will be updated in coming weeks.",
                "More information available on the Public Health Scotland website",
                tags$a(href="https://publichealthscotland.scot/news/2022/february/covid-19-reporting-to-include-further-data-on-reinfections/",
