32cecfe51f7bd7ee07195ffb0dbcae62783bab39
Seonaid More <seonam02@nssrstudio03.csa.scot.nhs.uk>
2022-04-01 15:49:27 +0100

    added new csv output

--- Dashboard Data Transfer/Transfer scripts/transfer_LFD.R
+++ Dashboard Data Transfer/Transfer scripts/transfer_LFD.R
@@ -13,12 +13,22 @@ g_lfd <- i_lfd$`NHS Board`
 
 write.csv(g_lfd, glue("Test output/LFD_Board.csv"), row.names = FALSE)
 
-### b) Weekly trend 
+### b) Weekly trend
 
-g_lfdtrend <- i_lfd$`Number of Tests Weekly` %>% 
+g_lfdtrend <- i_lfd$`Number of Tests Weekly` %>%
   dplyr::rename(`Week Ending` = week_ending,
                 `Number of LFD Tests` = n) %>% head(-1) # Removing last entry as not complete
 
 write.csv(g_lfdtrend, glue("Test output/LFD_Weekly.csv"), row.names = FALSE)
 
-rm(i_lfd, g_lfd)
+### c) Weekly trend by test group
+
+g_lfdtestgroup <- i_lfd$`` %>%
+  dplyr::rename(`Week Ending` = week_ending,
+                `Test Group` = test_group,
+                `Number of LFD Tests` = LFD_tests,
+                `Number of Positive Tests` = LFD_positives) #  %>% head(-1) is this needed?
+
+write.csv(g_lfdtestgroup, glue("Test output/LFD_TestGroup.csv"), row.names = FALSE)
+
+rm(i_lfd, g_lfd, g_lfdtestgroup)
