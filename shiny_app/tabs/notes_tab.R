72faf30f0fab8ff71b748a16ec4bdebd172fba1e
Seonaid More <seonam02@nssrstudio04.csa.scot.nhs.uk>
2022-05-27 14:41:20 +0100

    tab buttons should now work
    ethnicity requires data notes, vaccination status requires source - relevant people have been contacted from mailbox
    
    vaccination certification tab might have text on it that needs removed that's now in notes tab (I don't have it saved on my analyst space)

--- shiny_app/tabs/notes_tab.R
+++ shiny_app/tabs/notes_tab.R
@@ -19,7 +19,7 @@ output$cases_inf_notes <-renderUI({
       ". The number of daily confirmed cases may differ slightly from data published on this dashboard because the data has some
             cases added retrospectively and assigned to days based on the most up to date records. This has no impact on the overall
             number of confirmed cases."),
-    p(strong("Source: ECOSS")),
+    p(strong("Source: PCR - Electronic Communication of Surveillance in Scotland (ECOSS); LFD - UK Government self-reported / NSS Portal")),
     br(),
     p("Please note:"),
     tags$li("The number of daily confirmed cases may differ slightly from data published on the ",
@@ -41,7 +41,6 @@ output$cases_inf_notes <-renderUI({
       p("More information is available on the ",
         tags$a("Public Health Scotland website", href = "https://publichealthscotland.scot/news/2022/february/covid-19-reporting-to-include-further-data-on-reinfections/"),
         "."),
-    #p(strong("Source: ")),
     br(),
     bsButton("jump_to_inf_cases_n", label = "Go to tab"),
     bsButton("jump_to_inf_cases_data_n", label = "Go to data")
@@ -59,7 +58,7 @@ output$LFD_notes <-renderUI({
       those eligible for COVID treatments and those applying for the self-isolation support grant."),
     p("For information regarding LFD testing during term time as part of the Schools Asymptomatic Testing Programme, please visit the ",
       tags$a("COVID-19 Education Surveillance Report", href = "https://scotland.shinyapps.io/phs-covid19-education/_w_f53417bb/#tab-9333-2"), "."),
-    #p(strong("Source: ")),
+    p(strong("Source: UK Government self-reported / NSS Portal (health and social care cohorts)")),
     br(),
     p("Please note:"),
     tags$li("Test results and test group are self-reported, so numbers of tests and positives do not include tests which have not been registered on the UK Gov Portal."),
@@ -85,7 +84,7 @@ output$severe_illness_notes <-renderUI({
     h4("Hospital admissions"),
     p("These admissions are defined as: A patient's first positive PCR or LFD test of the episode of infection (including reinfections at 90 days or more after
       their last positive test) for COVID-19 up to 14 days prior to admission to hospital, on the day of their admission or during their stay in hospital."),
-    p(strong("Source: Rapid and Preliminary Inpatient Data (RAPID)")),
+    p(strong("Source: PCR - Electronic Communication of Surveillance in Scotland (ECOSS); LFD - UK Government self-reported / NSS Portal; Rapid and Preliminary Inpatient Data (RAPID)")),
     br(),
     p("Please note:"),
     tags$li("Episodes of reinfection were included from 01 March 2022, so care should be taken when interpreting trends over time. For more information, see ",
@@ -107,9 +106,12 @@ output$severe_illness_notes <-renderUI({
     br(),
 
     h4("Ethnicity"),
-    p("Please note that the data on hospital admissions by ethnicity only refers to laboratory confirmed (PCR) COVID-19 tests."),
+    # p(""),
+    # p(strong("Source:")),
+    # br(),
+    # p("Please note:"),
+    # tags$li(""),
     br(),
-
     h4("ICU admissions"),
     p("COVID-19 varies in severity from very mild symptoms through to those requiring hospital admission and the most ill who require intensive care treatment and
       supported ventilation in an Intensive Care Unit (ICU). Monitoring the admission frequency to critical care units in Scotland (ICU) is therefore an important
@@ -119,7 +121,7 @@ output$severe_illness_notes <-renderUI({
     tags$li("a valid linkage to laboratory data ", strong("AND")),
     tags$li("with laboratory confirmation for COVID-19 during the 21 days before the date of ICU admission ", strong("OR")),
     tags$li("with laboratory confirmation for COVID-19 during their ICU stay, from the date of ICU admission up to and including the date of ICU discharge."),
-    p(strong("Source: SICSAG")),
+    p(strong("Source: Scottish Intensive Care Society Audit Group (SICSAG)")),
     br(),
     p("Please note:"),
     tags$li("SICSAG data are stored in a dynamic database and subject to ongoing validations, so data may change weekly."),
@@ -141,14 +143,38 @@ output$severe_illness_notes <-renderUI({
 output$population_int_notes <-renderUI({
 
   tagList(
-    h4("Care homes"),
+    h3("Care homes"),
+    h4("Cases"),
     p("As of 06 April 2022, Public Health Scotland are reporting weekly data on COVID-19 cases in adult Care Homes in Scotland, previously reported by the ",
-      tags$a("Scottish Government", href = "https://www.gov.scot/publications/coronavirus-covid-19-data-for-scotland/"),
+      tags$a("Scottish Government",
+             href = "https://www.gov.scot/publications/coronavirus-covid-19-data-for-scotland/"),
       ". A positive case includes both new infections and possible reinfections, which are defined as individuals who test positive by PCR or LFD, 90 or more
-      clear days after their last positive test.",
-      p("")),
-    #p(strong("Source: ")),
+      clear days after their last positive test."),
+    p("PCR and LFD tests are linked to care homes through a variety of measures, dependent on the source. This includes the test centre ID, organisation ID
+        and organisation role. Residents and staff are identified using the organisation role and test reason."),
+    p(strong("Source: PCR - Electronic Communication of Surveillance in Scotland (ECOSS); LFD - UK Government self-reported / NSS Portal (health and social care cohorts)")),
     br(),
+    p("Please note:"),
+    tags$li("The source data are dynamic, and additional test results received will be reflected in future calculations of cases, which may affect figures retrospectively."),
+    br(),
+    h4("Care home visiting status in Scotland"),
+    p("As of 24 February 2021, care home visiting guidance was updated to allow care homes to update their own visiting plans depending on COVID-19 outbreak status.
+      Outbreak status is self-reported by care homes and capture status at the time of data extraction."),
+    p("Care homes report indoor visiting level as one of the following: Up to two visits per week, Daily visits, or Multiple visitors. Where indoor visiting is not possible,
+      outdoor and essential visits will be reported."),
+    p(strong("Source: Turas Care Home Management System")),
+    br(),
+    p("Please note:"),
+    tags$li("Data should be treated as provisional."),
+    tags$li("There may be a lag between a change in care home visiting guidance and a care home updating their corresponding visiting status in the tool. Therefore,
+            these data are to be interpreted as a guide, and may not be representative of current visiting status. For this reason, weekly data is not comparable."),
+    tags$li("As these data are self-reported, quality of information is dependent on accurate data entry, although there are some data checks for robustness."),
+    tags$li("Outbreak status reported may not match information from the Care Inspectorate (link) as timely data entry can be impacted by external factors."),
+    tags$li("Not all care homes submit information on a daily basis so there will be care homes where no information is available on a certain day, and this can vary considerably daily and weekly."),
+    tags$li("A small number of homes have not submitted outbreak data for an extended period, so data presented only include care homes that have submitted data in the last 30 days."),
+    tags$li("Where care homes have submitted data on multiple days within the time period, the latest status is reported."),
+    tags$li("“Other visiting status” represents outdoor visits only."),
+    tags$li("Data for individual NHS Health Boards reflect local circumstances. Therefore, caution is advised when drawing comparisons between boards."),
     bsButton("jump_to_pop_interest_n", label = "Go to tab")
     )
 }) # render UI close bracket
@@ -161,17 +187,20 @@ output$surveillance_notes <-renderUI({
     p("NHS24 is a phone line which provided support and medical advice to those concerned about coronavirus symptoms. The launch of the Redesign of Urgent Care programme
       has seen an increase in NHS24 activity from 01 December 2020 onwards. For more information on this programme, see ",
       tags$a("here", href = "https://www.gov.scot/policies/healthcare-standards/unscheduled-care/")),
-    #p(strong("Source:")),
+    p(strong("Source: NHS24 Systems Applications and Products Business Warehouse")),
     br(),
-   # p("Please note:"),
-   # tags$li(""),
-   # tags$li(""),
-   # br(),
+    p("Please note:"),
+    tags$li("From 15 September 2020, figures for the COVID helplineinclude calls made to the flu helpline. In late September, the first batch of flu vaccination letters
+            sent to those eligible identified by NHS Health Boards included the coronavirus number. The peaks in calls are consistent with the timing of those letters being
+            sent."),
+    tags$li("As of the 31 March 2022, the COVID-19 Community Pathway has closed. From this date, patients who contact NHS24 with COVID-19 symptoms during the in-hours period
+            will be advised to contact their own GP practice. This change will have an impact on the NHS24 data contained on the dashboard."),
+    br(),
     h4("Scottish Ambulance Service (SAS)"),
     p("SAS currently publish weekly unscheduled care operational statistics", tags$a("here.",
                                                                                      href = "https://www.scottishambulance.com/publications/unscheduled-care-operational-statistics/")),
       p("This publication details unscheduled care demand, response times and turnaround times."),
-    #p(strong("Source: ")),
+    p(strong("Source: SAS and Unscheduled Care Datamart")),
     br(),
     p("Please note:"),
     tags$li("Data published by SAS are sourced from a different operational system than that used for the Public Health Scotland (PHS) reporting, meaning that the data published by SAS will sometimes
@@ -189,12 +218,29 @@ output$surveillance_notes <-renderUI({
 output$vaccinations_notes <-renderUI({
 
   tagList(
-    #bsButton("jump_to_inf_cases_data", label = "Go to data"), # this is
-    #h3("Cases & infection levels"),
+    h4("COVID-19 vaccine certification"),
+    p("The NHS Covid Status App was launched on 30 September 2021. It is free and offers digital proof of vaccination via a QR code for each vaccination received. You can request a
+      vaccine certificate if you are aged 12 and over and have been vaccinated in Scotland."),
+    p("You can show your COVID-19 vaccine status by using the ", tags$a("NHS Scotland Covid Status app",
+                                                                        href = "https://www.nhsinform.scot/covid-status"),
+      ", download a PDF copy of your status from the app or ", tags$a("get a paper record of your vaccine status from NHS Inform",
+                                                                      href = "https://www.nhsinform.scot/nhs-scotland-covid-status/"), "."),
+    # p("Check the vaccine certification scheme guidance for ", tags$a("businesses and event organisers",
+    #                                                                  href = ""), "and for", tags$a("customers",
+    #                                                                                                href = ""), "."),
+    p("For further information, you can refer to the ", tags$a("Scottish Government website",
+                                                           href = "https://www.gov.scot/news/living-safely-with-covid/"), "."),
+    #p(strong("Source: ")), #tbc
+    br(),
+    p("Please note:"),
+    tags$li("Vaccine certifications are no longer legally required in Scotland, but the app will remain available for any business that wishes to continue certification
+            on a voluntary basis to reassure customers."),
+    tags$li("The record will not show any vaccinations given outside of Scotland."),
+    br(),
     h4("Vaccine wastage"),
     p("Given the scale of the Covid-19 vaccination programme, some vaccine wastage has been unavoidable for a variety of reasons including logistical issues,
       storage failure and specific clinical situations."),
-    #p(strong("Source: NSS Service Now wastage form")),
+    p(strong("Source: NSS Service Now wastage form")),
     br(),
     p("Please note:"),
     tags$li("The vaccine wastage form is populated by health board clinicians which can impact the timeliness and accuracy of the data."),
