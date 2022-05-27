615bbb38e92e33cf16235de4d0f121af9a5e29bb
RosalynLP <rosalyn.pearson@phs.scot>
2022-05-27 12:03:42 +0100

    reformat percentage

--- Dashboard Data Transfer/Transfer scripts/transfer_carehomes_sg.R
+++ Dashboard Data Transfer/Transfer scripts/transfer_carehomes_sg.R
@@ -9,7 +9,9 @@ g_notes <- i_ch$Notes
 
 write.csv(g_notes, glue(test_output, "CareHomeVisitsNotes.csv"), row.names = FALSE)
 
-g_board <- i_ch$`Table 1`
+g_board <- i_ch$`Table 1` %>%
+  mutate(`% supporting indoor visits (as a proportion of all homes)` = 100*`% supporting indoor visits (as a proportion of all homes)`,
+         `% supporting indoor visits (as a proportion of those returning a huddle)` = 100*`% supporting indoor visits (as a proportion of those returning a huddle)`)
 
 write.csv(g_board, glue(test_output, "CareHomeVisitsBoard.csv"), row.names = FALSE)
 
@@ -17,10 +19,18 @@ g_outbreak <- i_ch$`Table 2`
 
 write.csv(g_outbreak, glue(test_output, "CareHomeVisitsOutbreak.csv"), row.names = FALSE)
 
-g_board_older <- i_ch$`Table 3a`
+g_board_older <- i_ch$`Table 3a` %>%
+  mutate(`% supporting indoor visits (as a proportion of all homes)` = 100*`% supporting indoor visits (as a proportion of all homes)`,
+         `% supporting indoor visits (as a proportion of those returning a huddle)` = 100*`% supporting indoor visits (as a proportion of those returning a huddle)`)
+
 
 write.csv(g_board_older, glue(test_output, "CareHomeVisitsBoardOlder.csv"), row.names = FALSE)
 
-g_outbreak_older <- i_ch$`Table 3b`
+g_outbreak_older <- i_ch$`Table 3b` %>%
+  mutate(`% supporting indoor visits (as a proportion of all homes)` = 100*`% supporting indoor visits (as a proportion of all homes)`,
+         `% supporting indoor visits (as a proportion of those returning a huddle)` = 100*`% supporting indoor visits (as a proportion of those returning a huddle)`)
+
 
 write.csv(g_outbreak_older, glue(test_output, "CareHomeVisitsOutbreakOlder.csv"), row.names = FALSE)
+
+rm(g_notes, g_board, g_outbreak, g_board_older, g_outbreak_older, i_ch)
