9b632830f5feaf76ff9765a97bf7615a6f0311b8
RosalynLP <rosalyn.pearson@phs.scot>
2022-11-18 11:25:12 +0000

    update date

--- shiny_app/tabs/summary_tab.R
+++ shiny_app/tabs/summary_tab.R
@@ -479,7 +479,7 @@ if (data_explorer_selection() == "LabCases") { #Positive Cases
           # p("COVID-19 related admissions have been identified as the following: A patient may have tested positive for COVID-19 14 days prior to admission to hospital,
           #   on the day of their admission or during their stay in hospital."),
           # p("Please note that the data on hospital admissions by ethnicity only refers to laboratory confirmed (PCR) COVID-19 tests."),
-          p("These data will next be updated in December 2022."),
+          p("These data will next be updated in March 2023."),
           p("Click on legend to select or deselect categories: "),
           tags$li("Single click on an item in the legend to remove it from the plot"),
           tags$li("Double click on an item in the legend to view only that line"),
